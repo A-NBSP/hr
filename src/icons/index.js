a3c579245313908bd807f3b126d5d455146dbc5c
jr <1932933132@qq.com>
2022-09-23 18:19:04 +0800

    feat:初始化

---
+++ src/icons/index.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import SvgIcon from '@/components/SvgIcon'// svg component
+
+// register globally
+Vue.component('svg-icon', SvgIcon)
+// require.context（'导入文件的路径'，是否查找子目录，'匹配文件的正则'）:批量导入文件，返回一个方法
+// req.keys():导入文件的所有文件名数组
+const req = require.context('./svg', false, /\.svg$/)
+console.log(req.keys())
+console.log(req('./dashboard.svg'))
+console.log(req)
+const requireAll = requireContext => {
+  console.log(requireContext)
+  requireContext.keys().map(requireContext)
+}
+requireAll(req)
